093e2dadadd58b51e773ddd0a102e53b60d3640e
Шагин Евгений Александрович <shagine@okb.local>
2020-04-29 14:18:28 +0300

    Тест commit 4

--- src/app/Vowel.java
+++ src/app/Vowel.java
@@ -1,14 +1,16 @@
 package app;
 
-//import java.util.Scanner;
+import java.util.Scanner;
 
 public class Vowel {
 
     public static int count_vowel(String str) {
 
         char[] m_str = str.replaceAll(" ", "").toCharArray();
+
         int count = 0;
         char vowel[] = { 'a', 'e', 'i', 'o' };
+
         for (int i = 0; i < m_str.length; i++) {
             for (int j = 0; j < vowel.length; j++) {
                 if (m_str[i] == vowel[j]) {
@@ -17,13 +19,15 @@ public class Vowel {
                 }
             }
         }
+        System.out.println(m_str);
         return count;
     }
 
     public static void main(String[] args) {
-        // Scanner sc = new Scanner(System.in);
-        System.out.println(count_vowel("asdsdas  fas   fghio"));
-        ;
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        sc.close();
+        System.out.println(count_vowel(str));
 
     }
 
