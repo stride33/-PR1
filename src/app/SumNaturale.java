f66a8fd8d8b7f672dbc1fa2f6c5ceea67eee4be8
Шагин Евгений Александрович <shagine@okb.local>
2020-04-30 10:49:34 +0300

    Тест commit 7

--- src/app/SumNaturale.java
+++ src/app/SumNaturale.java
@@ -1,17 +1,29 @@
 package app;
 
 import java.util.stream.IntStream;
+import java.util.*;
 
 public class SumNaturale {
 
-    public static int Sum(int number) {
-        IntStream intStream = IntStream.range(0, number);
-        intStream.forEach(action);
-
-        return 0;
+    public static int[] getInRange(final int min, final int max, final int step) {
+        return IntStream.iterate(min, i -> i < max, i -> i + step).toArray();
     }
 
-    public static void main(String[] args) {
+    public static int Sum(int number) {
+        int count = 0;
+        int[] numberRange = getInRange(0, number, 1);
+        System.out.println(Arrays.toString(numberRange));
+        for (int i : numberRange) {
+            if ((i % 3 == 0) | (i % 5 == 0)) {
+                count += i;
+            } else
+                continue;
+        }
+        return count;
+    }
+
+    public static void main(final String[] args) {
+        System.out.println(Sum(1000));
 
     }
 }
