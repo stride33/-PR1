5124f2d21057bdf3b49c65bb0544c0053d4575e7
Шагин Евгений Александрович <shagine@okb.local>
2020-05-08 13:58:01 +0300

    TESTOVOE 123123

--- src/app/massString.java
+++ src/app/massString.java
@@ -2,10 +2,11 @@ package app;
 
 public class massString {
     public static void main(String[] args) {
-        String[] str = new String[3];
+        String[] str = new String[4];
         str[0] = "Welcom 1";
         str[1] = "Welcom 2";
         str[2] = "Welcom 3";
+        str[3] = "Welcom 3";
         for (String i : str) {
             System.out.println(i);
         }
